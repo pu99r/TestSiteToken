396d3d075fa8209440ccf4f650c7605f30e2876a
igor <iiigor.mik@gmail.com>
2024-02-08 14:30:25 +0300

    ts

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-// import { Route, Routes } from 'react-router-dom';
+import React from 'react';
 import NavBar from "../Components/NavBar/NavBar";
 import MainInfo from "../Components/MainInfo/MainInfo";
 
